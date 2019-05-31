5dd86ee17e621e030a0297191b37577cacec0908
Vinícius Seixas <vinicius.oliveira@ccc.ufcg.edu.br>
2019-05-30 23:07:01 -0300

    create_disk_file simplified

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-import os
+from os.path import exists 
 from mkfs import mkfs
 
 FILE_NAME = 'disk'
 FILE_SIZE = 20 * 1024 * 1024
 
 def create_disk_file():
-    with open(FILE_NAME, 'wb') as f:
-        f.write(os.urandom(FILE_SIZE))
+  f = open(FILE_NAME, 'wb')
+  f.write("\0" * FILE_SIZE)
 
 def disk_file_exists():
-    return os.path.exists(FILE_NAME)
+  return exists(FILE_NAME)
 
 def setup():
-    if not disk_file_exists(): create_disk_file()
-    mkfs(FILE_NAME)
+  if not disk_file_exists(): create_disk_file()
+  mkfs(FILE_NAME)
 
 setup()
