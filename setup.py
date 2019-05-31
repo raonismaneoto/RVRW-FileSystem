3bf84219541f57d37b06bc7aeee861ff3425a32c
Vinícius Seixas <vinicius.oliveira@ccc.ufcg.edu.br>
2019-05-30 21:29:26 -0300

    file system setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from mkfs import mkfs
+
+FILE_NAME = 'disk'
+FILE_SIZE = 20 * 1024 * 1024
+
+def create_disk_file():
+    with open(FILE_NAME, 'wb') as f:
+        f.write(os.urandom(FILE_SIZE))
+
+def disk_file_exists():
+    return os.path.exists(FILE_NAME)
+
+if not disk_file_exists(): create_disk_file()
+mkfs(FILE_NAME)
+
