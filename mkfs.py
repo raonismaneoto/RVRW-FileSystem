d28594ff6550a4d2e39f8e8ec0bbfe8337d5f2c4
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2019-06-10 21:27:38 -0300

    saving the objects

--- mkfs.py
+++ mkfs.py
@@ -1,6 +1,8 @@
 from super_block import SuperBlock
 from inode import Inode
 from block import Block
+import os
+import pickle
 
 SIZE = 20*1024
 
@@ -12,7 +14,7 @@ def mkfs(disk_path):
 
 	super_block = SuperBlock(SIZE, 100, f_blocks_list, 15, inodes_list)
 
-	save(super_block, disk_path)
+	save_from(super_block, '$', disk_path)
 
 
 def create_f_blocks_list():
@@ -21,6 +23,7 @@ def create_f_blocks_list():
 	for i in xrange(100):
 		current.next = Block()
 		current = current.next
+		save_from(current, '@', 'disk')
 	return head
 
 def create_inodes_list():
@@ -29,7 +32,24 @@ def create_inodes_list():
 	for i in xrange(15):
 		current.next = Inode()
 		current = current.next
+		save_from(current, '!', 'disk')
 	return head
 
 def save(sb, disk_path):
-	pass
+	import pickle
+	with open(disk_path, "wb") as disk:
+		disk.seek(0, os.SEEK_SET)
+		pickle.dump(sb, disk)
+		disk.close()
+
+def save_from(object, mark_char, disk_path):
+	with open(disk_path, "rw+") as disk:
+		byte = 0
+		for line in disk:
+			for char in line:
+				if char == mark_char:
+					byte = disk.tell()
+					disk.seek(byte, os.SEEK_SET)
+					pickle.dump(object, disk)
+					disk.close()
+					return
